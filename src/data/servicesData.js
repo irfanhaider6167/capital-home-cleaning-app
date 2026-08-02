fe7fe1a1830e64f73fbe47cb6cd50b2c7a691609
irfanhaider6167 <101730155+irfanhaider6167@users.noreply.github.com>
2026-08-02 22:41:33 +0500

    Update Service Details.js

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -119,9 +119,9 @@ export const services = [
   },
 
   {
-    slug: "plumbring",
-    title: "Plumbring",
-    desc: "Capital Cleaning & Maintenance provides professional Plumbring services in Islamabad & Rawalpindi including leak repairs, pipe installation, bathroom fittings, kitchen plumbring, and maintenance solutions for homes and offices.",
+    slug: "plumbing",
+    title: "Plumbing",
+    desc: "Capital Cleaning & Maintenance provides professional Plumbing services in Islamabad & Rawalpindi including leak repairs, pipe installation, bathroom fittings, kitchen plumbing, and maintenance solutions for homes and offices.",
     hero: "/images/plumbring-a-final.jpg",
     before: "/images/plumbring-b-final.jpg",
     after: "/images/plumbring-a-final.jpg",
